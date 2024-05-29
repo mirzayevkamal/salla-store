39101d55a6bec276e5c94a42ea0ac098ee003d06
mirzayevkamal <kamalmirzayev@Kamals-MacBook-Air.local>
2024-05-29 23:29:21 +0400

    Update readme file

--- src/actions/getProducts.ts
+++ src/actions/getProducts.ts
@@ -4,8 +4,18 @@ import { IProduct } from "@/types/global";
 
 export const getProducts = async (limit?: number, sort?: string) => {
   try {
+    const sortQuery = sort?.length ? `&sort=${sort}` : "";
+    const limitQuery = limit ? `?limit=${limit}` : "";
+
+    console.log("sortQuery", typeof sort);
+    console.log("limitQuery", typeof limit);
+
     const res = await fetch(
-      `https://fakestoreapi.com/products?limit=${limit}&sort=${sort}`
+      `https://fakestoreapi.com/products${limitQuery}${sortQuery}`,
+      {
+        cache: "no-store",
+        method: "GET",
+      }
     );
     const data = (await res.json()) as IProduct[];
     return data;
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/logo";
 
 export default function Loading() {
   return (
-    <div className="animate-spin w-full h-[80vh] flex items-center justify-center">
+    <div className="animate-spin w-full flex items-center justify-center">
       <Logo width={100} height={100} />
     </div>
   );
